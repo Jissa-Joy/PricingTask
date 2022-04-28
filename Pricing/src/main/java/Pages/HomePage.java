a78252ffba0ad0827aa556d6db4c167eea8afb9b
Jissa <jisaelza@gmail.com>
2022-04-28 15:56:55 +1000

    second commit

--- Pricing/src/main/java/Pages/HomePage.java
+++ Pricing/src/main/java/Pages/HomePage.java
@@ -1,35 +1,50 @@
 package Pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-import base.TestBase;
 
-public class HomePage extends TestBase {
-	
 
-	//driver.findElement(By.linkText("Standard pricing")).click();
-	@FindBy(linkText="Standard pricing")
-	WebElement link;
+public class HomePage {
 	
-	public HomePage() {
+  private static String pageUrl = "https://www.google.com.au";
+  private WebDriver driver ;
+  
+  //locators
+	//name=q
+	
+  @FindBy(name="q")
+	WebElement searchbar;
+  
+  @FindBy(name="btnK")
+  WebElement searchButton;
+	
+	//Constructor
+	public HomePage(WebDriver driver) {
+		this.driver = driver;
+		driver.get(pageUrl);
 		PageFactory.initElements(driver,this);
+		
 	}
 	
-	//Actions:
-		public String validatePricingTitle(){
-			return driver.getTitle();
-		}
+	
+	
+	public void ClickSearch()
+	{
+		searchbar.sendKeys("SecurePay");
+		searchbar.sendKeys(Keys.ENTER);
+	WebElement result = pageUrl.find_element_by_tag_name("h3");
+		result.click();
+	
 		
-	//	public Poli()
-		{
-			
-			WebElement learnMore = driver.findElement(By.xpath("//*[@id=\"gatsby-focus-wrapper\"]/main/section[3]/div[2]/div/div/div/div/div[1]/div/a"));
-			((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView();", learnMore);
-			
-		//	return new Poli();
-		}
+		
+		
+	}
+	
+	
 
 }
--- Pricing/src/main/java/Pages/StandardPricing.java
+++ Pricing/src/main/java/Pages/StandardPricing.java
@@ -1,5 +1,35 @@
 package Pages;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 
 public class StandardPricing {
+	 private WebDriver driver ;
+	
+	@FindBy(className="link")
+	WebElement stdPricing;
+	
+	
+	//click standards pricing link
+	
+	public void Enter() {
+	stdPricing.click();
+	
+	}
+	
+	//locate Learn More button 
+	/*[@id="gatsby-focus-wrapper"]/main/section[3]/div[2]/div/div/div/div/div[1]/div/a */
+	WebElement Element = driver.findElement(By.xpath("*[@id=\"gatsby-focus-wrapper\"]/main/section[3]/div[2]/div/div/div/div/div[1]/div/a"));
+	
+	 /* Scroll down the page till element is found */
+	
+	JavascriptExecutor js = (JavascriptExecutor)driver;
+	js.executeScript("arguments[0].scrollIntoView()", Element);
+	
+	
 
 }
